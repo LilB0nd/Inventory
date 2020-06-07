bd9fe1bbe2d526b5dcc99c40d80a367fc00a27b1
LilB0nd <57658534+LilB0nd@users.noreply.github.com>
2020-06-07 16:30:12 +0200

    - views machen

--- inventory_managementsentials/management/commands/import_rooms.py
+++ inventory_managementsentials/management/commands/import_rooms.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-#from polls.models import Question as Poll
+from inventory_managementsentials.models import Room
 
 
 class Command(BaseCommand):
@@ -7,4 +7,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write("Import rooms")
+        room_file = open("./inventory_managementsentials/management/commands/sql_raeume.csv")
+        print(Room.objects.all())
+        for line in room_file:
+            room_number = line.split(',')[1]
 
+            room = Room(description=room_number)
+            room.save()
+
+            self.stdout.write("Imported room : {}".format(room_number))
--- inventory_managementsentials/views.py
+++ inventory_managementsentials/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Room
+from .models import Room, Computer
 
 
 class IndexView(generic.ListView):
@@ -15,7 +15,8 @@ class IndexView(generic.ListView):
 
 def room(request, room_id):
     room_list = Room.objects.all()
+    computerlist = Computer.objects.all()
     context = {'room_list': room_list[0]}
-    return HttpResponse(room_list)
+    return HttpResponse(room_list, computerlist)
     # output = ", ".join([Room.description for Room in room_list])
     # return render(request, 'inventory_managementsentials/index.html', context)
